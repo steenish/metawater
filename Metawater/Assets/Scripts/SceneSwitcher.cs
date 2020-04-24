16718e832b1e9fa987e09633ed1838632683dc2e
Anders Steen <anders.steen98@gmail.com>
2020-04-24 23:27:25 +0200

    make unity native additions

---
+++ Metawater/Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour {
+
+  public void LoadFullVersion() {
+    SceneManager.LoadScene(0);
+  }
+
+  public void LoadUnityCollisionsVersion() {
+    SceneManager.LoadScene(1);
+  }
+
+  public void LoadUnityCollisionsNileVersion() {
+    SceneManager.LoadScene(2);
+  }
+}
